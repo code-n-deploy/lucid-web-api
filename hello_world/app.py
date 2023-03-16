ccd667d429dabe1b982bdd9d25429d40017f168e
Maxwell <alexander.s.maxwell@gmail.com>
2023-03-16 11:47:47 -0400

    Testing pipeline

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Hello Motto",
+            "message": "Hello Everyday People",
             # "location": ip.text.replace("\n", "")
         }),
     }
